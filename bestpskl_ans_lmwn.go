9632883ea83420e194f413699d0ea65ff5a80497
Pasakorn Limchuchua <66000440+bestpskl@users.noreply.github.com>
2022-07-14 20:00:33 +0700

    add answer file

---
+++ bestpskl_ans_lmwn.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func reverse(s string) string {
+	rns := []rune(s) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	return string(rns)
+}
+
+func main() {
+
+	// var whatIsIt string
+
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	str := string(sd)
+	ans := reverse(str)
+	fmt.Println(ans)
+}
